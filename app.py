5ed14ea2721a3dd6cc8cbe6a0e1aa3ccef8aa03d
ogoogo2778-collab <ogoogo2778@gmail.com>
2026-05-23 17:41:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import streamlit as st
-import tensorflow as tf
 import numpy as np
 from PIL import Image
+import gdown
+import os
+import tensorflow as tf
 
 st.title("Deteksi Kematangan Durian AI")
 
-model = tf.keras.models.load_model("model.keras")
+MODEL_PATH = "model.keras"
+
+# download model jika belum ada
+if not os.path.exists(MODEL_PATH):
+    url = "https://drive.google.com/uc?id=1-gTWOCSauzsXtjRWI3Yd-6lrEzaDbXe0"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+# load model SEKALI saja
+model = tf.keras.models.load_model(MODEL_PATH)
 
 uploaded_file = st.file_uploader("Upload gambar durian")
 
@@ -13,20 +23,9 @@ if uploaded_file:
     img = Image.open(uploaded_file)
     st.image(img)
 
-    img = img.resize((150,150))
+    img = img.resize((150, 150))
     img = np.array(img) / 255.0
     img = np.expand_dims(img, axis=0)
 
     pred = model.predict(img)
     st.write("Hasil prediksi:", pred)
-import gdown
-import os
-import tensorflow as tf
-
-MODEL_PATH = "model.keras"
-
-if not os.path.exists(MODEL_PATH):
-    url = "https://drive.google.com/uc?id=1-gTWOCSauzsXtjRWI3Yd-6lrEzaDbXe0"
-    gdown.download(url, MODEL_PATH, quiet=False)
-
-model = tf.keras.models.load_model(MODEL_PATH)
