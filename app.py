b874cf052d043f3d8f81e5c9b4a2e9d121048f7e
ogoogo2778-collab <ogoogo2778@gmail.com>
2026-05-23 17:44:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
 import streamlit as st
-import numpy as np
 from PIL import Image
-import gdown
-import os
-import tensorflow as tf
+import numpy as np
 
 st.title("Deteksi Kematangan Durian AI")
 
-MODEL_PATH = "model.keras"
-
-# download model jika belum ada
-if not os.path.exists(MODEL_PATH):
-    url = "https://drive.google.com/uc?id=1-gTWOCSauzsXtjRWI3Yd-6lrEzaDbXe0"
-    gdown.download(url, MODEL_PATH, quiet=False)
-
-# load model SEKALI saja
-model = tf.keras.models.load_model(MODEL_PATH)
+st.write("Web sudah online (mode demo)")
 
 uploaded_file = st.file_uploader("Upload gambar durian")
 
 if uploaded_file:
     img = Image.open(uploaded_file)
-    st.image(img)
-
-    img = img.resize((150, 150))
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
-
-    pred = model.predict(img)
-    st.write("Hasil prediksi:", pred)
+    st.image(img, caption="Gambar diterima")
+    st.write("Mode AI sementara dimatikan agar web stabil")
