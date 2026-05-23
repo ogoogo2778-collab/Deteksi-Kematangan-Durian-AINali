bff32526e08c347beecc24fda9a2d545e1c08be8
ogoogo2778-collab <ogoogo2778@gmail.com>
2026-05-23 18:03:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 
-st.title("🍈 Deteksi Kematangan Durian AI")
+st.title("🍈 Deteksi Kematangan Durian AINali")
 
 uploaded_file = st.file_uploader("Upload gambar durian", type=["jpg", "png", "jpeg"])
 
@@ -10,17 +10,23 @@ if uploaded_file:
     img = Image.open(uploaded_file)
     st.image(img, caption="Gambar masuk")
 
-    st.write("🔍 Analisis AI sedang berjalan...")
+    st.write("🔍 Analisis AINali sedang berjalan...")
 
-    # simulasi hasil (biar tetap ada output)
+    # simulasi AI
     score = np.random.rand()
 
     if score < 0.33:
         hasil = "Mentah"
+        estimasi_hari = np.random.randint(3, 7)
+        status = f"Perkiraan matang dalam ± {estimasi_hari} hari"
     elif score < 0.66:
         hasil = "Matang"
+        status = "Sudah siap dikonsumsi 🍈"
     else:
         hasil = "Busuk"
+        status = "Tidak layak konsumsi ❌"
 
+    # OUTPUT (HARUS DI LUAR IF/ELSE)
     st.success(f"Hasil Prediksi: {hasil}")
+    st.info(status)
     st.write("Confidence:", round(score, 2))
