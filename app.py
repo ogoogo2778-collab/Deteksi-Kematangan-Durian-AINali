e638d36ef11ca70663484d6dd64106eb958ca5d2
ogoogo2778-collab <ogoogo2778@gmail.com>
2026-05-23 17:27:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+st.title("Deteksi Kematangan Durian AI")
+
+model = tf.keras.models.load_model("model.keras")
+
+uploaded_file = st.file_uploader("Upload gambar durian")
+
+if uploaded_file:
+    img = Image.open(uploaded_file)
+    st.image(img)
+
+    img = img.resize((150,150))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    pred = model.predict(img)
+    st.write("Hasil prediksi:", pred)
 import gdown
 import os
 import tensorflow as tf
